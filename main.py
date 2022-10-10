d3331192e41cb064b334d0de758791fb08689f96
vladyslavius <marakhovskyiv@gmail.com>
2022-10-11 01:22:12 +0300

    :sparkles: Added run configuration

--- main.py
+++ main.py
@@ -15,14 +15,15 @@ log = logging.getLogger()
 logging.basicConfig(level=logging.DEBUG)
 
 
-
 class UserProtocol(Protocol):
     login: T_LOGIN
     password: T_PASSWORD
 
+
 class User(NamedTuple):
     login: str
     password: str
+
     def __str__(self):
         print(f"{self.login}: {self.password}")
 
@@ -30,9 +31,8 @@ class User(NamedTuple):
 def validate(users: list[UserProtocol], amount: int) -> None:
     logins = set(map(lambda user: user.login, users))
     if amount != (amount_of_logins := len(logins)):
-        raise ValueError(
-            f'Not enough of unique items. Required: "{amount}". Provided: "{amount_of_logins}"'
-        )
+        raise ValueError(f'Not enough of unique items. Required: "{amount}". Provided: "{amount_of_logins}"')
+
 
 def generate_login(key: int | None = None):
     login = faker.user_name()
@@ -41,11 +41,13 @@ def generate_login(key: int | None = None):
 
     return login
 
+
 def generate_password():
     characters = string.ascii_letters + string.digits + string.punctuation
     password = "".join(random.choice(characters) for _ in range(random.randint(0, 16)))
 
     return password
+
 
 def generate_users(amount: int) -> Iterator[User]:
     logins = set()
@@ -54,13 +56,14 @@ def generate_users(amount: int) -> Iterator[User]:
         login = generate_login(key=amount)
 
         if login in logins:
-            log.info(f'-------------------------------------------------------------- non-unique user: {(login)}. Total non-unique: {counter}')
+            log.info(
+                f"-------------------------------------------- non-unique user: {(login)}. Total non-unique: {counter}"
+            )
             counter += 1
             continue
 
         logins.add(login)
-        log.info(f'Generated unique combination of login and password for user: {len(logins)}')
-
+        log.info(f"Generated unique combination of login and password for user: {len(logins)}")
 
         yield User(
             login=login,
